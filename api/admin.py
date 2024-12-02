44d0d3fc2a73ef2482c6a381608646f994381c65
ferigeek <150931210+ferigeek@users.noreply.github.com>
2024-12-02 19:02:13 +0330

    created models

---
+++ api/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+# Register your models here.
+
+class Admin(UserAdmin):
+    model = models.User
+    list_display = ['email', 'username', 'is_staff', 'is_active']
+    search_fields = ['email', 'username']
+    ordering = ['email']
+
+admin.site.register(models.User, Admin)    
+admin.site.register(models.Category)
---
+++ api/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        if self.first_name is not None:
+            return f'{self.first_name} - {self.last_name}'
+        else:
+            return self.email 
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    view_count = models.IntegerField(default=0)
+    publish_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=1000)
+    author = models.ForeignKey(User, on_delete=models.SET('Deleted Account'))
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
+    
